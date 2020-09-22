21af207182e528e2b3516ea22b15a5c62e60449e
Nick Pfisterer <nick@polymoon.net>
2020-09-21 17:38:18 -0700

    Add lecture channels to config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 const config = {
   commands: {
     defaultPrefix: '!'
+  },
+  lectures: {
+    channels: {
+      lectureQueue: '757670685374939308',
+      lectureChat: '757670343635894302'
+    }
   }
 }
 
