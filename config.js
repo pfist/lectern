205c0b44507f29a8a42e8048f49204d7398316f6
Nick Pfisterer <nick@polymoon.net>
2020-09-25 10:21:05 -0700

    Add production channel IDs

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const config = {
   },
   lectures: {
     channels: {
-      lectureQueue: '757670685374939308',
-      lectureChat: '757670343635894302'
+      lectureQueue: '759095914886987816', // Dev = 757670685374939308, Prod = 759095914886987816
+      lectureChat: '739543683191406742' // Dev = 757670343635894302, Prod = 739543683191406742
     }
   }
 }
