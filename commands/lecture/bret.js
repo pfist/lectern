980e29badcf556b08287409691c0d9eb10316ca6
Nick Pfisterer <nick@polymoon.net>
2021-02-14 09:07:12 -0800

    Add better error logging for Bret command

--- commands/lecture/bret.js
+++ commands/lecture/bret.js
@@ -19,33 +19,41 @@ class BretCommand extends Command {
   async exec (message) {
     const channel = await this.client.channels.cache.get(this.client.config.lectures.channels.lectureQueue)
     const messages = await channel.messages.fetch()
-    const filtered = await messages.filter(message => message.embeds.length !== 0 && message.embeds[0].footer.text === 'Breakfast with Bret')
+    const messagesWithEmbeds = await messages.filter(message => message.embeds.length !== 0)
+    this.client.log.debug(`${messagesWithEmbeds.size} messages with embeds found.`)
 
-    if (filtered.size >= 1) {
-      const queueMessage = filtered.first()
-      const oldEmbed = queueMessage.embeds[0]
-      const queue = queueMessage.embeds[0].fields[1].value.split('\n')
+    if (messagesWithEmbeds.size >= 1) {
+      const messagesWithQueues = await messagesWithEmbeds.filter(message => message.embeds[0].footer.text === 'Breakfast with Bret')
+      this.client.log.debug(`${messagesWithQueues.size} messages with queues found.`)
 
-      if (queue.includes(message.member.displayName)) {
-        return message.reply('You are already in the queue.')
+      if (messagesWithQueues.size >= 1) {
+        const queueMessage = messagesWithQueues.first()
+        const oldEmbed = queueMessage.embeds[0]
+        const queue = queueMessage.embeds[0].fields[1].value.split('\n')
+
+        if (queue.includes(message.member.displayName)) {
+          return message.reply('You are already in the queue.')
+        }
+
+        if (queue.includes('─')) {
+          queue.pop()
+        }
+
+        queue.push(message.member.displayName)
+
+        const newEmbed = this.client.util.embed(oldEmbed)
+          .spliceFields(1, 1, {
+            name: ':point_up: Waiting to Speak',
+            value: queue
+          })
+
+        queueMessage.edit(newEmbed)
+        return message.reply('Your request was added to the queue.')
+      } else {
+        return message.util.send('Bret isn\'t here right now. :cry:')
       }
-
-      if (queue.includes('─')) {
-        queue.pop()
-      }
-
-      queue.push(message.member.displayName)
-
-      const newEmbed = this.client.util.embed(oldEmbed)
-        .spliceFields(1, 1, {
-          name: ':point_up: Waiting to Speak',
-          value: queue
-        })
-
-      queueMessage.edit(newEmbed)
-      return message.reply('Your request was added to the queue.')
     } else {
-      return message.util.send('Bret isn\'t here right now. :cry:')
+      this.client.log.error('No messages with embeds found!')
     }
   }
 }
